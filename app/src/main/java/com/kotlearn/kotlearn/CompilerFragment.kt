9df37d92982619af28513fb219157f2797b4ae7e
Quan Le <quan_le@u.nus.edu>
2018-11-04 03:04:55 +0800

    Add fragments

---
+++ app/src/main/java/com/kotlearn/kotlearn/CompilerFragment.kt
@@ -0,0 +1,12 @@
+package com.kotlearn.kotlearn
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CompilerFragment : android.support.v4.app.Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_compiler, container, false)
+    }
+}
--- app/src/main/java/com/kotlearn/kotlearn/MainActivity.kt
+++ app/src/main/java/com/kotlearn/kotlearn/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kotlearn.kotlearn
 
+import android.app.Fragment
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
@@ -8,21 +9,29 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        var selectedFragment: android.support.v4.app.Fragment? = null
         when (item.itemId) {
             R.id.navigation_profile -> {
-                message.setText("helo")
-                return@OnNavigationItemSelectedListener true
+//                message.setText("helo")
+                selectedFragment = ProfileFragment()
+//                return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_content -> {
-                message.setText("Content")
-                return@OnNavigationItemSelectedListener true
+//                message.setText("Content")
+                selectedFragment = ContentFragment()
+//                return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_complier -> {
-                message.setText("Complier")
-                return@OnNavigationItemSelectedListener true
+//                message.setText("Complier")
+                selectedFragment = CompilerFragment()
+//                return@OnNavigationItemSelectedListener true
             }
         }
-        false
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, selectedFragment)
+        transaction.commit()
+        return@OnNavigationItemSelectedListener true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +39,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, ProfileFragment()).commit()
     }
 }
