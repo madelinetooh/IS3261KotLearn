58b49f3b5568687656df5eb4c6a8f598a51b74c1
Quan Le <quan_le@u.nus.edu>
2018-11-08 20:55:20 +0800

    Add activities

---
+++ app/src/main/java/com/kotlearn/kotlearn/ContentPageActivity.kt
@@ -0,0 +1,12 @@
+package com.kotlearn.kotlearn
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ContentPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_content_page)
+    }
+}
